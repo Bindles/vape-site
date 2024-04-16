d96e803eae4c848a558ac10f59d4c5500bfa8a1d
Bindles <bindlez@gmail.com>
2024-04-16 15:25:18 -0400

    stimulus-collection-td

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root "posts#index"
+  resources :posts
+  
+  get 'index2' => 'posts#index2'
+  get 'index3' => 'posts#index3'
+  get 'index4' => 'posts#index4'
+  get 'index5' => 'posts#index5test'
+  get 'index6' => 'posts#index6'
+  get 'test' => 'posts#test'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
